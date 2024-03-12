b98ba0f718bfd7cb111ed907ad0b184264903bcd
m00nwtchr <m00nwtchr@duck.com>
2024-03-12 21:50:50 +0100

    buildscript

--- crates/cofd-miner/build.rs
+++ crates/cofd-miner/build.rs
@@ -11,10 +11,10 @@ use cofd_meta::SourceMeta;
 
 #[cfg(feature = "embed_meta")]
 fn embed_meta() -> Result<()> {
-	println!("cargo:rerun-if-changed=../meta");
+	println!("cargo:rerun-if-changed=../../meta");
 	let out_dir = env::var("OUT_DIR").unwrap();
 	let dest_path = Path::new(&out_dir).join("meta.bin");
-	let vec: Vec<SourceMeta> = fs::read_dir("../meta")
+	let vec: Vec<SourceMeta> = fs::read_dir("../../meta")
 		.unwrap()
 		.filter_map(|entry| entry.ok().map(|e| e.path()))
 		.filter(|path| path.extension().map(|ext| ext.eq("json")).unwrap_or(false))
