18d933ecd82ec46e15908c0f12b81a0889ef9b9c
m00nwtchr <m00nwtchr@duck.com>
2024-12-24 02:29:40 +0100

    update

--- crates/codex-scraper/src/gifts.rs
+++ crates/codex-scraper/src/gifts.rs
@@ -7,7 +7,7 @@ use cofd_schema::{
 		gift::{Facet, Gift, GiftKind, Moon, Other},
 		Item,
 	},
-	splat::werewolf::Renown,
+	template::werewolf::Renown,
 };
 
 use crate::MultiMap;
@@ -81,7 +81,7 @@ pub fn parse_gifts(map: MultiMap) -> anyhow::Result<(Vec<Gift<Moon>>, Vec<Gift<O
 								inner: Facet {
 									inner: Moon {
 										level: dots_to_num(&str).unwrap_or(0),
-										auspice: cofd_schema::splat::werewolf::Auspice::Cahalith,
+										auspice: cofd_schema::template::werewolf::Auspice::Cahalith,
 									},
 									action: /*if !(cost.is_empty()
 										&& pool.is_empty() && action.is_empty()
