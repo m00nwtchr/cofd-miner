6e17f29ec8e0424ff52b52883cca4f6b824ab0ed
m00nwtchr <m00nwtchr@duck.com>
2025-01-05 14:49:31 +0100

    req

--- crates/miner/examples/req.rs
+++ crates/miner/examples/req.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{error::Error, fs};
 
 use reqwest::Client;
 use serde_json::{json, Value};
@@ -6,51 +6,58 @@ use serde_json::{json, Value};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	// Read the JSON schema from file
-	let schema_content = fs::read_to_string("schema.json")?;
-	let schema: Value = serde_json::from_str(&schema_content)?;
+	let schema: Value = serde_json::from_str(include_str!("../../../llm/schema.json"))?;
 
-	// Read the unstructured text from file
-	let text_content = fs::read_to_string("gifts.txt")?;
+	let text = include_str!("../../../llm/gifts.txt").trim().split("\n\n");
 
+	for text in text {
+		let json = extract(&schema, text).await?;
+
+		// Print the response
+		println!("{}", serde_json::to_string_pretty(&json)?);
+	}
+	// println!("{}", response.text().await?);
+
+	Ok(())
+}
+
+async fn extract(schema: &Value, text: &str) -> Result<Value, Box<dyn Error>> {
 	// Construct the prompt
-	let prompt = format!(
-        "The following is unstructured text and a known JSON schema. Extract relevant information from the text and map it to the schema. Ensure the data types match the schema.\n\nSchema: {}\n\nText: {}\n\nStructured JSON:",
-        text_content.trim(),
-        schema
-    );
+	let prompt = format!("Text: \n{}\n\nSchema: \n```json\n{}\n```\n\n", text, schema);
 
 	// Create the JSON payload
 	let payload = json!({
-		"model": "llama3.2:3b",
 		"messages": [
+			{
+				"role": "system",
+				"content": "You will receive some text and a JSON schema. Extract relevant information from the text and map it to the schema. Ensure the data types match the schema.",
+			},
 			{
 				"role": "user",
 				"content": prompt,
 			}
 		],
-		// "prompt": prompt,
-		"format": schema,
+		"response_format": {
+			"type": "json_schema",
+			"json_schema": schema,
+		},
 		"stream": false,
 	});
 
 	// Send the POST request to the Ollama API
 	let client = Client::new();
 	let response = client
-		.post("http://localhost:11434/api/chat")
+		.post("http://localhost:8080/v1/chat/completions")
 		.json(&payload)
 		.send()
 		.await?;
 
 	// Parse the response as JSON
 	let response_json: Value = response.json().await?;
-	println!("{:#?}", response_json);
-	let model_response = response_json["message"]["content"].as_str().unwrap();
+	// println!("{:#?}", response_json);
+	let model_response = response_json["choices"][0]["message"]["content"]
+		.as_str()
+		.unwrap();
 	let json: Value = serde_json::from_str(model_response)?;
-
-	// Print the response
-	println!("{}", serde_json::to_string_pretty(&json)?);
-
-	// println!("{}", response.text().await?);
-
-	Ok(())
+	Ok(json)
 }
